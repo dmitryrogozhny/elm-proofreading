ec1932936ddcda3dd1d9d8b99ceba663d5f8b430
d.rogozhny@gmail.com <d.rogozhny@gmail.com>
2017-08-29 11:07:24 +0300

    Add 04-embedding-elm-in-html

---
+++ 04-embedding-elm-in-html/Main.elm
@@ -0,0 +1,35 @@
+import Html exposing (div, text, textarea)
+import Html.Attributes exposing (class, value)
+import Html.Events exposing (onInput)
+
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+type alias Model =
+    { text : String
+    }
+
+model : Model
+model =
+    Model ""
+
+-- UPDATE
+type Msg
+    = SetText String
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        SetText text ->
+            { model | text = text }
+
+
+-- VIEW
+view : Model -> Html.Html Msg
+view model =
+    div []
+        [ textarea [ onInput SetText, value model.text, class "text-editor" ] []
+        , div [ class "proofread-panel" ] [ text model.text ]
+        ]
